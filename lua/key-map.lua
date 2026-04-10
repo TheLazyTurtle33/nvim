cdc8fc0657f878b525994034c8c301030786c0c9
lazyturtle33 <lazyturtle3333@gmail.com>
2026-04-10 18:26:41 +0200

    disabled arrow keys

--- lua/key-map.lua
+++ lua/key-map.lua
@@ -4,6 +4,24 @@ vim.keymap.set("v", "<C-_>", "gc", { remap = true, desc = 'Toggle comment' })
 vim.keymap.set("n", "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+local modes = { 'n', 'i', 'v' }
+local keys = { '<Up>', '<Down>', '<Left>', '<Right>' }
+
+-- disable arrow keys
+vim.keymap.set('n', '<Up>',    '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Down>',  '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Left>',  '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Right>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('i', '<Up>',    '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('i', '<Down>',  '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('i', '<Left>',  '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('i', '<Right>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('v', '<Up>',    '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('v', '<Down>',  '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('v', '<Left>',  '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('v', '<Right>', '<Nop>', { noremap = true, silent = true })
+
+
 -- telescope
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
